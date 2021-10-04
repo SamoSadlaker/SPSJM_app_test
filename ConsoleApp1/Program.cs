1dc321c1ee697488428826092ffdbaa1b78d8651
SamoSadlaker <samkosadlak@gmail.com>
2021-10-04 20:40:38 +0200

    Upgraded MiniGame

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,33 +4,62 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+        static bool isRunning;
+        static bool guessed(int num)
         {
-            System.Random random = new System.Random();
-            Console.WriteLine("LOL Hádaj číslo");
-            int num = random.Next(100);
-            
-           repeat:
             Console.WriteLine("Napíš číslo:");
             int guess = Convert.ToInt32(Console.ReadLine());
-
-            if (num > guess) 
+            if (num > guess)
             {
                 Console.WriteLine("Je to viac XD");
-                goto repeat;
             }
             else if (num < guess)
             {
                 Console.WriteLine("Je to menej LOL");
-                goto repeat;
             }
             else if (num == guess)
             {
-                goto done;               
+                return true;
+            }
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Chceš si zahrať hru? [y / n]");
+            string response = Console.ReadLine();
+
+            if (response == "y")
+            {
+                isRunning = true;
+            }
+            else if (response == "n")
+            {
+                isRunning = false;
             }
 
-            done:
-            Console.WriteLine("Nice");
+            if (isRunning)
+            {
+                System.Random random = new System.Random();
+                Console.WriteLine("LOL Hádaj číslo");
+                int num = random.Next(100);
+
+
+                if (guessed(num))
+                {
+                    Console.WriteLine("Nice");
+                }
+
+                while (isRunning)
+                {
+                    if (guessed(num))
+                    {
+                        Console.WriteLine("Nice");
+                        isRunning = false;
+
+                    }
+                }
+            }
+
         }
             
     }
