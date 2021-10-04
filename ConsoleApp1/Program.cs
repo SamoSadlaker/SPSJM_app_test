d0faa2ce9c304f7aee7f3e96e22ca1c8b00ebe71
SamoSadlaker <samkosadlak@gmail.com>
2021-10-04 17:21:14 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Random random = new System.Random();
+            Console.WriteLine("LOL Hádaj číslo");
+            int num = random.Next(100);
+            
+           repeat:
+            Console.WriteLine("Napíš číslo:");
+            int guess = Convert.ToInt32(Console.ReadLine());
+
+            if (num > guess) 
+            {
+                Console.WriteLine("Je to viac XD");
+                goto repeat;
+            }
+            else if (num < guess)
+            {
+                Console.WriteLine("Je to menej LOL");
+                goto repeat;
+            }
+            else if (num == guess)
+            {
+                goto done;               
+            }
+
+            done:
+            Console.WriteLine("Nice");
+        }
+            
+    }
+}
+
