cf81c87302a2245d52225384ccee8a39e41dd613
SamoSadlaker <samkosadlak@gmail.com>
2021-10-06 21:31:28 +0200

    Colors and Messages

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,15 +7,34 @@ namespace ConsoleApp1
         static bool isRunning;
         static bool guessed(int num)
         {
-            Console.WriteLine("Napíš číslo:");
-            int guess = Convert.ToInt32(Console.ReadLine());
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine();
+            Console.WriteLine("══ Skús napísať číslo na ktoré myslím ══");
+            Console.ForegroundColor = ConsoleColor.White;
+            string input = Console.ReadLine();
+            int guess;
+            while (!int.TryParse(input, out guess))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine();
+                Console.WriteLine("══ Toto nieje číslo! ══");
+                Console.WriteLine("    Skús to znovu ");
+                Console.ForegroundColor = ConsoleColor.White;
+                input = Console.ReadLine();
+            }
+
+            
             if (num > guess)
             {
-                Console.WriteLine("Je to viac XD");
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.WriteLine();
+                Console.WriteLine("» Myslím na väčšie číslo :D «");
             }
             else if (num < guess)
             {
-                Console.WriteLine("Je to menej LOL");
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.WriteLine("» Myslím na menšie číslo :D «");
             }
             else if (num == guess)
             {
@@ -25,36 +44,42 @@ namespace ConsoleApp1
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("Chceš si zahrať hru? [y / n]");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("╔══ Chceš si zahrať hru? [y / n] ══╗");
+            Console.ForegroundColor = ConsoleColor.White;
             string response = Console.ReadLine();
 
             if (response == "y")
             {
                 isRunning = true;
             }
-            else if (response == "n")
+            else
             {
                 isRunning = false;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine();
+                Console.WriteLine("Tak nie no :(");
+                Console.WriteLine();
             }
 
             if (isRunning)
             {
                 System.Random random = new System.Random();
-                Console.WriteLine("LOL Hádaj číslo");
-                int num = random.Next(100);
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                int num = random.Next(5);
 
 
-                if (guessed(num))
-                {
-                    Console.WriteLine("Nice");
-                }
-
-                while (isRunning)
+                while (true)
                 {
                     if (guessed(num))
                     {
-                        Console.WriteLine("Nice");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine();
+                        Console.WriteLine("╚══ Číslo si uhádol :) ══╝");
+                        Console.WriteLine();
+                        Console.ForegroundColor = ConsoleColor.White;
                         isRunning = false;
+                        break;
 
                     }
                 }
